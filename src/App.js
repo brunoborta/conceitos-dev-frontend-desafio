126fe264b3ae2a7005d62d55336c4d8f502048ec
Bruno Bortagaray <bruno.bortagaray@justwatch.com>
2020-12-06 17:46:05 +0100

    Adicionada a funcionalidade de listagem de repos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function loadRepositories() {
+      const repositories = await api.get("repositories");
+      setRepositories(repositories.data);
+    }
+    loadRepositories();
+  }, []);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,13 +26,12 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(({ id, title, url }) => (
+          <li key={id}>
+            <a href={url}>{title}</a>
+            <button onClick={() => handleRemoveRepository(id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
